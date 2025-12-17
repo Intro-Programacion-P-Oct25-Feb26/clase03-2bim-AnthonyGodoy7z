f208a7e46d71ec587d762e24aaad07a9f9fb98f7
Anthony <tonyxiz10@gmail.com>
2025-12-17 12:44:30 -0500

    2

--- Ejemplosc3/src/paquete05/Ejercicio01.java
+++ Ejemplosc3/src/paquete05/Ejercicio01.java
@@ -4,10 +4,32 @@
  */
 package paquete05;
 
+import java.util.Scanner;
+
 /**
  *
  * @author UTPL
  */
 public class Ejercicio01 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String[] nombres = new String[5];
+        double[] produccion = new double[5];
+        String name;
+        double produ;
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println("Ingrese nombre del trabajador");
+            name = entrada.nextLine();
+            nombres[i] = name;
+            
+            System.out.println("Ingrese produccion en dolares");
+            produ = entrada.nextDouble();
+            produccion[i] = produ;
+            
+        }
+
+    }
+
+}
 }
