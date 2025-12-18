19700c85a017877b216a12886cfbc859a63c2e78
Anthony <tonyxiz10@gmail.com>
2025-12-18 01:19:18 -0500

    commit

--- Ejemplosc3/src/paquete05/Ejercicio01.java
+++ Ejemplosc3/src/paquete05/Ejercicio01.java
@@ -15,26 +15,52 @@ public class Ejercicio01 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         String[] nombres = new String[5];
-        double[] produccion = new double[5];
-        String name;
-        double produ;
-        double promedio;
+        double[][] produccion = new double[5][12];
+        double[] total = new double[5];
+        double mayorProd;
+        int mayor;
+        String mensaje;
         for (int i = 0; i < nombres.length; i++) {
             System.out.println("Ingrese nombre del trabajador");
-            name = entrada.nextLine();
-            entrada.nextDouble();
-            nombres[i] = name;
-
-            System.out.println("Ingrese produccion en dolares");
-            produ = entrada.nextDouble();
-            entrada.nextLine();
-            produccion[i] = produ;
+            nombres[i] = entrada.nextLine();
+            for (int j = 0; j < produccion[i].length; j++){
+                System.out.printf("Ingrese produccion del mes (%d) en "
+                        + "dolares:", j+1);
+                produccion[i][j] = entrada.nextDouble();
+                entrada.nextLine();
+                
+                total[i] = total[i] + produccion[i][j];
+            }
         }
-        for (int j = 0; j < produccion[j]; j++) {
-            produccion[j]
-            
-            
+        
+        mayorProd = total[0];
+        mayor = 0;
+        
+        for (int i = 1; i < total.length; i++){
+            if(total[i] > mayorProd){
+                mayorProd = total[i];
+                mayor = i;
+            }
         }
+        
+        mensaje = String.format("\nAnálisis de Producción\nEstación\n");
+        
+        for (int i = 0; i < total.length; i++){
+            mensaje = String.format("%sNombre Estación %d - Total producción: "
+                    + "$ %.2f\n", 
+                    mensaje,
+                    i + 1,
+                    total[i]);
+        }
+        
+        mensaje = String.format("%s\nEstación más productiva: Nombre Estación "
+                + "%d\nEncargado de la estación: %s\n"
+                + "Cantidad de la estación más productiva: $%.2f", 
+                mensaje,
+                mayor + 1, 
+                nombres[mayor],
+                mayorProd);
+        
+        System.out.printf(mensaje);
     }
-
 }
